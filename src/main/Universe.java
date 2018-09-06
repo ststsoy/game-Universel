330f6008a4d12ef83347a2d7a2f0ee24ec45ab2a
ststsoy <ststsoy@yahoo.com>
2018-09-06 15:43:05 +0600

    NEW file n

--- src/main/Universe.java
+++ src/main/Universe.java
@@ -16,134 +16,144 @@ import java.security.Key;
 
 public class Universe extends JFrame implements KeyListener {
     private static Universe fit;
-    private static Image karable;
     private static Image bacground;
-    private static Image bullet;
-    private static Image enamy;
-    private static Image enamy_bullet;
-    private static long  last_frame_time = System.nanoTime();
+    private static Image karable; //Корабль игрока
+    private static Image bullet;// Пуля игрока
+    private static Image enamy;   // Корабль врага
+    private static Image enamy_bullet;  // Пуля врага
+    private static long last_frame_time = System.nanoTime ();
     private static Pole vip;
-    private static float x=200;
-    private static float y=500;
-    private static float v=200;
-    private static float top=200;
-    private static float botom=700;
-    private static float enamy_top=-100;
-    private static float enamy_botom=200;
-    private static float enamy_bullet_top=-100;
-    private static float enamy_bullet_botom=200;
+    private static float v = 200;// Скорость игрока
+    private static float v_e = 400; // Скорость врага
+    private static float x_k = 200; // Кордината X игрока корабля
+    private static float y_k = 500; // Кордината Y игрока корабля
+    private static float x_k_b = 200;// Кордината X игрока пули
+    private static float y_k_b = 500; // Кордината Y игрока пули
+    private static float x_e = -100; // Кордината X врага корабля
+    private static float y_e = 200; // Кордината Y врага корабля
+    private static float x_e_b = -100;// Кордината X врага пули
+    private static float y_e_b = 200;// Кордината X врага пули
 
-    public Universe(){
-        addKeyListener(this);
+    public Universe () {
+        addKeyListener (this);
     }
-    public static void main(String[] args) throws IOException {
-        fit = new Universe();
-        fit.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        fit.setTitle("Uneverse");
-        fit.setSize(800, 700);
 
-        vip = new Pole();
-        last_frame_time = System.nanoTime();
-        vip.setSize(800, 700);;
-        vip.setVisible(true);
-        bacground = ImageIO.read(Universe.class.getResourceAsStream("oboik.ru_60754.jpg"));
-        karable = ImageIO.read(Universe.class.getResourceAsStream("player_2_straight_idle.gif"));
-        bullet=ImageIO.read(Universe.class.getResourceAsStream("261-iconimg.png"));
-        enamy_bullet=ImageIO.read(Universe.class.getResourceAsStream("enamy_bullet.png"));
-        enamy=ImageIO.read(Universe.class.getResourceAsStream("ename.png"));
-        fit.add(vip, BorderLayout.CENTER);
-        fit.setVisible(true);
+    public static void main (String[] args) throws IOException {
+        fit = new Universe ();
+        fit.setDefaultCloseOperation (WindowConstants.EXIT_ON_CLOSE);
+        fit.setTitle ("Uneverse");
+        fit.setSize (800, 700);
 
-        }
+        vip = new Pole ();
+        last_frame_time = System.nanoTime ();
+        vip.setSize (800, 700);
+
+        vip.setVisible (true);
+        bacground = ImageIO.read (Universe.class.getResourceAsStream ("oboik.ru_60754.jpg"));
+        karable = ImageIO.read (Universe.class.getResourceAsStream ("karable.png"));
+        bullet = ImageIO.read (Universe.class.getResourceAsStream ("261-iconimg.png"));
+        enamy = ImageIO.read (Universe.class.getResourceAsStream ("ename.png"));
+        enamy_bullet = ImageIO.read (Universe.class.getResourceAsStream ("enamy_bullet.png"));
+        fit.add (vip, BorderLayout.CENTER);
+        fit.setVisible (true);
 
 
-
+    }
 
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped (KeyEvent e) {
         //JOptionPane.showMessageDialog(null,e.getKeyText(e.getKeyChar()));
-
-
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == 39 && x<700) {
-            x+=30;
-            getGraphics().drawImage(karable, (int) x,(int) y, null);
-        }else if(e.getKeyCode() == 37&& x>0)
-            x-=30;
-            getGraphics().drawImage(karable,(int) x,(int) y, null);
-        if(e.getKeyCode()==40 && y<530) {
-            y+=30;
-            getGraphics().drawImage(karable,(int) x,(int) y, null);
-        }else if(e.getKeyCode()==38 && y>0){
-            y-=30;
-            getGraphics().drawImage(karable,(int) x,(int) y,null);
+    public void keyPressed (KeyEvent e) {
+        if (e.getKeyCode () == 39 && x_k < 700) {
+            x_k += 30;
+            getGraphics ().drawImage (karable, (int) x_k, (int) y_k, null);
+        } else if (e.getKeyCode () == 37 && x_k > 0)
+            x_k -= 30;
+        getGraphics ().drawImage (karable, (int) x_k, (int) y_k, null);
+        if (e.getKeyCode () == 40 && y_k < 530) {
+            y_k += 30;
+            getGraphics ().drawImage (karable, (int) x_k, (int) y_k, null);
+        } else if (e.getKeyCode () == 38 && y_k > 0) {
+            y_k -= 30;
+            getGraphics ().drawImage (karable, (int) x_k, (int) y_k, null);
         }
-        if (e.getKeyCode() == 68) {
-             int i=0;
-            while (i<100){
-                botom--;
-                getGraphics().drawImage(bullet,(int)top+((int) karable.getWidth(null)/2), (int) botom,null);
-                if(botom == 0){
-                    botom=y;
-                    top=x;
-                }
-                i++;
-                repaint();
-                }
 
-        }}
+        if (e.getKeyCode () == 68) {
+
+            //g.drawImage(enamy, (int) enamy_top, (int) enamy_botom, null);
+
+
+        }
+}
 
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased (KeyEvent e) {
         //JOptionPane.showMessageDialog(null,e.getKeyCode());
-
     }
-    private static void render(Graphics g){
 
-    }
-    private static void onRepaint(Graphics g) {
-
-
-        long current_time = System.nanoTime();
+    private static void onRepaint (Graphics g) {
+        long current_time = System.nanoTime ();
         float delta_time = (current_time - last_frame_time) * 0.000000001f;
         last_frame_time = current_time;
-        enamy_botom = enamy_botom + v * delta_time;
-        g.drawImage(bacground, 0, 0, null);
-        g.drawImage(karable, (int) x, (int) y, null);
-        g.drawImage(enamy, (int) enamy_top, (int) enamy_botom, null);
-
-            if (enamy_botom >700) {
-
-                enamy_botom = -100;
-                enamy_top = (int) (Math.random() * (vip.getWidth() - enamy.getWidth(null)));
-            }
-
-            enamy_bullet_botom +=2;
-            g.drawImage(enamy_bullet,(int)enamy_bullet_top+((int) enamy.getHeight(null)/2), (int)enamy_bullet_botom ,null);
-            if(enamy_bullet_botom >700){
-                enamy_bullet_botom=enamy_botom;
-                enamy_bullet_top=enamy_top;
-
+        g.drawImage (bacground, 0, 0, null);
+        g.drawImage (karable, (int) x_k, (int) y_k, null);
+        y_e = y_e + v * delta_time;
+        g.drawImage (enamy, (int) x_e, (int) y_e, null);
+        y_e_b = y_e_b + v_e * delta_time;
+        g.drawImage (enamy_bullet, (int) x_e_b + ((int) enamy.getHeight (null) / 2 - 30), (int) y_e_b, null);
+        if (y_e > vip.getHeight ()) {
+            y_e =-100;
+            x_e = (int) (Math.random () * (vip.getWidth () - enamy.getWidth (null)));
+        }
+        if(y_e_b >vip.getHeight()){
+             x_e_b=x_e;
+             y_e_b=y_e;
+        }
+        float x_k_0=x_k;
+        float y_k_0=y_k+karable.getWidth(null);
+        float x_k_1=x_k+karable.getHeight(null);
+        float y_k_1=y_k+karable.getWidth(null);
+        float x_k_2=x_k+karable.getHeight(null);
+        float y_k_2=y_k;
+        float x_e_b_c = x_e_b+(enamy_bullet.getWidth (null)/2);
+        float y_e_b_c = y_e_b+(enamy_bullet.getHeight (null)/2);
+        boolean is_bullet2= x_k<=x_e_b_c&&y_k<=y_e_b_c&&x_k_1>=x_e_b_c&&y_k_1>=y_e_b_c;
+        if(is_bullet2){
+           x_k=700;
+           y_k=500;
+        }
+        y_k_b--;
+        g.drawImage (bullet, (int) x_k_b + (karable.getWidth (null) / 2)-20, (int) y_k_b, null);
+        if (y_k_b == 0) {
+            x_k_b = x_k;
+            y_k_b = y_k;
+        }
+        float x_e_0 = x_e;
+        float y_e_0 = y_e + enamy.getWidth (null);
+        float x_e_1 = x_e + enamy.getHeight (null);
+        float y_e_1 = y_e + enamy.getWidth (null);
+        float x_e_2 = x_e + enamy.getHeight (null);
+        float y_e_2 = y_e;
+        float x_k_b_c = x_k_b +(bullet.getHeight (null) / 2);
+        float y_k_b_c = y_k_b + (bullet.getWidth (null) / 2);
+        boolean is_bullet = x_e<=x_k_b_c&&y_e<=y_k_b_c&&x_e_1>=x_k_b_c&&y_e_1>=y_k_b_c;
+        g.drawLine ((int) x_e, (int) y_e, (int) x_e_1, (int) y_e_1);
+        if (is_bullet) {
+            y_e = (int) -100;
+            x_e = (int) Math.random () * (fit.getWidth () - enamy.getWidth (null));
         }
 
-
-
-
-
-
-        //long current_time = System.nanoTime();
-        //float delta_time = (current_time - last_frame_time) * 0.000000001f;
-        //last_frame_time = current_time;
-        //botom = botom + v * delta_time;
-//
-        //g.drawImage(bullet, (int) top, (int) botom ,null);
+        g.drawLine((int) x_k,(int) y_k,(int) x_k_1,(int) y_k_1);
     }
-    private static class Pole extends JPanel {
+
+
+    public static class Pole extends JPanel {
+
 
         @Override
         protected void paintComponent(Graphics g) {
@@ -151,6 +161,9 @@ public class Universe extends JFrame implements KeyListener {
             onRepaint(g);
             repaint();
 
-            }
+
         }
+
+
+    }
 }
