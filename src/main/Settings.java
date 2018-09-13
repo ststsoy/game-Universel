b615c172c11c6c8d8236ddbfe9ea255f2c853d85
ststsoy <ststsoy@yahoo.com>
2018-09-13 22:18:44 +0600

    hi

---
+++ src/main/Settings.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Settings extends JFrame {
+
+    public Settings(){
+    }
+    public static class Panel extends JPanel{
+
+    }
+
+
+}
--- src/main/Universe.java
+++ src/main/Universe.java
@@ -13,6 +13,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.security.Key;
+import java.util.jar.JarEntry;
 
 public class Universe extends JFrame implements KeyListener {
     private static Universe fit;
@@ -20,33 +21,66 @@ public class Universe extends JFrame implements KeyListener {
     private static Image karable; //Корабль игрока
     private static Image bullet;// Пуля игрока
     private static Image enamy;   // Корабль врага
-    private static Image enamy_bullet;  // Пуля врага
+    private static Image enamy_bullet;  // Пуля враг а
     private static long last_frame_time = System.nanoTime ();
     private static Pole vip;
     private static float v = 200;// Скорость игрока
     private static float v_e = 400; // Скорость врага
-    private static float x_k = 200; // Кордината X игрока корабля
-    private static float y_k = 500; // Кордината Y игрока корабля
+    private static float x_k=200; // Кордината X игрока корабля
+    private static float y_k=500; // Кордината Y игрока корабля
     private static float x_k_b = 200;// Кордината X игрока пули
     private static float y_k_b = 500; // Кордината Y игрока пули
     private static float x_e = -100; // Кордината X врага корабля
     private static float y_e = 200; // Кордината Y врага корабля
     private static float x_e_b = -100;// Кордината X врага пули
     private static float y_e_b = 200;// Кордината X врага пули
-
+    private static Settings settings;
+    private static JTextArea textArea;
+    private static JLabel label;
+    private static int score_k;
+    private static int score_e;
     public Universe () {
         addKeyListener (this);
     }
 
     public static void main (String[] args) throws IOException {
+        settings = new Settings ();
+        settings.setDefaultCloseOperation (WindowConstants.EXIT_ON_CLOSE);
+        settings.setTitle ("Settings");
+        settings.setSize (800,700);
+        Settings.Panel panel = new Settings.Panel ();
+        panel.setSize (100,400);
+        panel.setLocation (100,200);
+        JButton button = new JButton ();
+        button.setSize (100,50);
+        button.setLabel ("Start");
+
+        button.addActionListener (new ActionListener () {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                push();
+
+            }
+        });
+        textArea = new JTextArea ();
+        textArea.setSize (100,200);
+        label =new JLabel ("Hello!!!");
+        panel.add(button,BorderLayout.NORTH);
+        panel.add (textArea);
+        panel.add (label);
+        settings.add(panel,BorderLayout.WEST);
+        settings.setVisible (true);
         fit = new Universe ();
         fit.setDefaultCloseOperation (WindowConstants.EXIT_ON_CLOSE);
         fit.setTitle ("Uneverse");
         fit.setSize (800, 700);
+        //GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        //d.setFullScreenWindow(fit);
+        //fit.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         vip = new Pole ();
         last_frame_time = System.nanoTime ();
-        vip.setSize (800, 700);
+        vip.setSize (fit.getWidth (), fit.getHeight ());
 
         vip.setVisible (true);
         bacground = ImageIO.read (Universe.class.getResourceAsStream ("oboik.ru_60754.jpg"));
@@ -55,11 +89,12 @@ public class Universe extends JFrame implements KeyListener {
         enamy = ImageIO.read (Universe.class.getResourceAsStream ("ename.png"));
         enamy_bullet = ImageIO.read (Universe.class.getResourceAsStream ("enamy_bullet.png"));
         fit.add (vip, BorderLayout.CENTER);
-        fit.setVisible (true);
-
-
+        fit.setVisible (false);
+        System.out.println (fit.getHeight ()+" "+fit.getWidth ());
     }
-
+    private static void push(){
+        fit.setVisible (true);
+    }
 
     @Override
     public void keyTyped (KeyEvent e) {
@@ -96,12 +131,13 @@ public class Universe extends JFrame implements KeyListener {
         //JOptionPane.showMessageDialog(null,e.getKeyCode());
     }
 
+
     private static void onRepaint (Graphics g) {
         long current_time = System.nanoTime ();
         float delta_time = (current_time - last_frame_time) * 0.000000001f;
         last_frame_time = current_time;
         g.drawImage (bacground, 0, 0, null);
-        g.drawImage (karable, (int) x_k, (int) y_k, null);
+        g.drawImage (karable, (int) x_k, (int)y_k, null);
         y_e = y_e + v * delta_time;
         g.drawImage (enamy, (int) x_e, (int) y_e, null);
         y_e_b = y_e_b + v_e * delta_time;
@@ -126,6 +162,14 @@ public class Universe extends JFrame implements KeyListener {
         if(is_bullet2){
            x_k=700;
            y_k=500;
+           score_e++;
+           if(score_e==3){
+               fit.setVisible (false);
+               label.setText ("You lose!!!");
+               score_e=0;
+               x_e_b=x_e;
+               y_e_b=y_e;
+           }
         }
         y_k_b--;
         g.drawImage (bullet, (int) x_k_b + (karable.getWidth (null) / 2)-20, (int) y_k_b, null);
@@ -142,13 +186,22 @@ public class Universe extends JFrame implements KeyListener {
         float x_k_b_c = x_k_b +(bullet.getHeight (null) / 2);
         float y_k_b_c = y_k_b + (bullet.getWidth (null) / 2);
         boolean is_bullet = x_e<=x_k_b_c&&y_e<=y_k_b_c&&x_e_1>=x_k_b_c&&y_e_1>=y_k_b_c;
-        g.drawLine ((int) x_e, (int) y_e, (int) x_e_1, (int) y_e_1);
+
         if (is_bullet) {
             y_e = (int) -100;
             x_e = (int) Math.random () * (fit.getWidth () - enamy.getWidth (null));
-        }
 
-        g.drawLine((int) x_k,(int) y_k,(int) x_k_1,(int) y_k_1);
+            x_k_b = x_k;
+            y_k_b = y_k;
+            score_k++;
+
+            if(score_k==3){
+                fit.setVisible (false);
+               settings.setTitle ("You win");
+               label.setText ("You win!!!!");
+               score_k=0;
+            }
+        }
     }
 
 
